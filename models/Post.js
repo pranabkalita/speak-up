6ced318df60d6eb8c3ee8babab14f924b3e71106
Pranab Kalita <pavankalita@gmail.com>
2021-06-02 14:53:14 +0530

    Implemented the slug generation with model hooks

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 const { Schema } = mongoose
 
 const postSchema = new Schema(
   {
-    title: String,
-    slug: String,
+    title: { type: String, unique: true },
+    slug: { type: String, unique: true },
     body: String,
     coverImage: String,
     images: Array,
@@ -15,6 +16,13 @@ const postSchema = new Schema(
     timestamps: { currentTime: () => Math.floor(Date.now() / 1000) },
   }
 )
+
+// Document Middleware: runs before .save() and .create()
+postSchema.pre('save', function (next) {
+  this.slug = slugify(this.title, { lower: true })
+
+  next()
+})
 
 const Post = mongoose.model('Post', postSchema)
 
