c39d9549b2ca4b1e84b756603129f6bf76a76683
Pranab Kalita <pavankalita@gmail.com>
2021-06-02 12:07:14 +0530

    Added the Post model.

--- server.js
+++ server.js
@@ -14,6 +14,23 @@ mongoose
     console.warn('ERROR : ', err)
   })
 
+const postSchema = new Schema(
+  {
+    title: String,
+    slug: String,
+    body: String,
+    coverImage: String,
+    images: Array,
+    isDraft: { type: Boolean, default: false },
+    isPublished: { type: Boolean, default: false },
+  },
+  {
+    timestamps: { currentTime: () => Math.floor(Date.now() / 1000) },
+  }
+)
+
+const Post = mongoose.model('Post', postSchema)
+
 const app = express()
 
 app.get('/', (req, res) => {
