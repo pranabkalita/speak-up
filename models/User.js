443285494c2bc537a135fc512749b76a1f3060f4
Pranab Kalita <pavankalita@gmail.com>
2021-06-02 22:54:25 +0530

    Implemented the password hashing before saving.

--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
+const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     aboutMe: String,
     photo: { type: String },
@@ -14,6 +15,17 @@ const userSchema = new Schema(
     timestamps: { currentTime: () => Math.floor(Date.now() / 1000) },
   }
 )
+
+/** Hash the password before saving to database */
+userSchema.pre('save', async function (next) {
+  // Only if the password is modified
+  if (!this.isModified('password')) return next()
+
+  // Hash the password with the cost of 12
+  this.password = await bcrypt.hash(this.password, 12)
+
+  next()
+})
 
 const User = mongoose.model('User', userSchema)
 
