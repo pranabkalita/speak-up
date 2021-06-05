bcbfad8c7c7fbc8e6a5c3ee456667f9187acbc8c
Pranab Kalita <pavankalita@gmail.com>
2021-06-05 17:28:35 +0530

    Implemented the auth protect middleware.

---
+++ middlewares/auth.js
@@ -0,0 +1,42 @@
+const { promisify } = require('util')
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/User')
+
+exports.protect = async (req, res, next) => {
+  // 1) Get the token and check if it exists
+  let token
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1]
+  }
+
+  if (!token) {
+    return res.status(401).json({
+      status: 'fail',
+      data: {
+        message: 'Not authorized !',
+      },
+    })
+  }
+
+  // 2) Verify the token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+
+  // 3) Check if the user still exists and not deleted
+  const freshUser = await User.findById(decoded.id)
+
+  if (!freshUser) {
+    return res.status(401).json({
+      status: 'fail',
+      data: {
+        message: 'The user no longer exists!',
+      },
+    })
+  }
+
+  // 4) Grant access to the route
+  next()
+}
--- routes/Post.js
+++ routes/Post.js
@@ -6,12 +6,20 @@ const postController = require('../controllers/PostController')
 // Validators
 const postValidator = require('./../validators/post')
 
+// Middlewares
+const authMiddleware = require('./../middlewares/auth')
+
 const router = express.Router()
 
 router.get('/', postController.all)
 router.get('/:slug', postController.getOne)
-router.post('/', postValidator.createValidator, postController.create)
-router.patch('/:id', postController.update)
-router.delete('/:id', postController.delete)
+router.post(
+  '/',
+  authMiddleware.protect,
+  postValidator.createValidator,
+  postController.create
+)
+router.patch('/:id', authMiddleware.protect, postController.update)
+router.delete('/:id', authMiddleware.protect, postController.delete)
 
 module.exports = router
